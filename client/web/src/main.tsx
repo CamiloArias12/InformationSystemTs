b74ea872db9589bf4b395d544305ea23fa8fa12b
CamiloArias12 <ariaspira13@gmail.com>
2023-05-30 18:01:50 -0500

    Apolo

--- client/web/src/main.tsx
+++ client/web/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { ApolloClient, InMemoryCache,ApolloProvider,gql } from '@apollo/client'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+
+const router =createBrowserRouter([
+   {
+      path:"/",
+      element: <App/>,
+   },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )

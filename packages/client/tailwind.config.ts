c02e0cf0b002fdf43c07ac97c71d3bb0bc2ecd92
CamiloArias12 <ariaspira13@gmail.com>
2023-07-19 17:28:48 -0500

    Add parameters

--- packages/client/tailwind.config.ts
+++ packages/client/tailwind.config.ts
@@ -6,7 +6,17 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+	 'img-bg':"url('https://www.iamcountryside.com/wp-content/uploads/2019/03/AdobeStock_126414595-scaled-e1666730922274.jpeg')",
+	 'img-profile':"url('https://www.pngkit.com/png/detail/281-2812821_user-account-management-logo-user-icon-png.png')"
+      },
+      height:{
+	 'dash':'cal(100vh - 10px)'
+
+      }
+
+    },
   },
   plugins: [],
 }
